17a6cbe97375f9164881db73633bf37a9e70086d
bianca.marostica <bmarostica@gmail.com>
2021-12-02 17:03:54 -0300

    biblioteca api, email e relatorio consumer

---
+++ relatorioconsumer/src/main/java/com/dbc/relatorioconsumer/RelatorioconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.relatorioconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatorioconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelatorioconsumerApplication.class, args);
+    }
+
+}
