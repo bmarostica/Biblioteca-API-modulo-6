c6fcc569a4e7e9cde1471041d93cc5f01c04ba1b
bianca.marostica <bmarostica@gmail.com>
2021-12-05 18:12:48 -0300

    email consumer

---
+++ emailconsumer/src/main/java/com/dbc/emailconsumer/dto/EmailDTO.java
@@ -0,0 +1,12 @@
+package com.dbc.emailconsumer.dto;
+
+import lombok.Data;
+
+@Data
+public class EmailDTO {
+
+    private String destinatario;
+    private String assunto;
+    private String texto;
+
+}
