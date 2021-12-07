f94a8fdf5fcb9890425c0f9b1ce4f81b07ca1aec
bianca.marostica <bmarostica@gmail.com>
2021-12-07 17:07:18 -0300

    livroService test outro test

--- Biblioteca-API/src/test/java/com/dbc/biblioteca/service/LivroServiceTest.java
+++ Biblioteca-API/src/test/java/com/dbc/biblioteca/service/LivroServiceTest.java
@@ -1,21 +1,18 @@
 package com.dbc.biblioteca.service;
 
+import com.dbc.biblioteca.dto.LivroCreateDTO;
+import com.dbc.biblioteca.dto.LivroDTO;
 import com.dbc.biblioteca.entity.LivroEntity;
 import com.dbc.biblioteca.exceptions.RegraDeNegocioException;
 import com.dbc.biblioteca.repository.LivroRepository;
-import com.dbc.biblioteca.service.LivroService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.*;
-import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
-
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 
@@ -44,6 +41,27 @@ public class LivroServiceTest {
         livroService.delete(5);
         verify(livroRepository, Mockito.times(1)).delete(livro);
 
+        //assert
+    }
+
+    @Test
+    public void createLivro() throws RegraDeNegocioException {
+        LivroEntity livro = new LivroEntity();
+        LivroDTO livroDTO = new LivroDTO();
+        LivroCreateDTO livroCreateDTO = new LivroCreateDTO();
+
+
+        livroService.create(livroCreateDTO);
+        when(objectMapper.convertValue(livroCreateDTO, LivroEntity.class)).thenReturn(livro);
+        when(livro.setData_registro()).then(livro.setData_registro(LocalDate.now()));
+
+        doReturn(Optional.of(livro)).when(livroRepository).save(livro);
+        //when(livroRepository.save(livro).thenReturn(Optional.of(livro));
+
+        when(objectMapper.convertValue(livro, LivroDTO.class)).thenReturn(livroDTO);
+
+
+        verify(livroRepository, Mockito.times(1)).save(livro);
     }
 
 
